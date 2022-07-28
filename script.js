8f57936ba25df219514ccaf232e33109e5c0d45f
unknown <cem4uk25@gmail.com>
2022-07-28 17:33:30 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?','');
+
+const personalMobieDB = {
+    count: numberOfFilms,
+    movies: {},
+    ectors: {},
+    ganres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его ?', ''),  
+      c = prompt('Один из просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его ?', '');
+      
+personalMobieDB.movies[a] = b;
+personalMobieDB.movies[c] = d;
+
+console.log(personalMobieDB);
+
+
+
+
